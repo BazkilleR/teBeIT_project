8bc85ac4b4de01a84b353f4df844ebbed7165fbf
BazkilleR <ohm.ratchaphon@gmail.com>
2023-11-28 15:41:52 +0700

    Add order into database

---
+++ src/addOrderDb.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+require "server.php";
+
+if (!isset($_POST["submit"])) {
+    header("Location: addOrder.php?error=error #1");
+    exit();
+}
+
+if (empty($_POST["product"]) || empty($_POST["amount"])) {
+    header("Location: addOrder.php?error=error #2");
+    exit();
+}
+
+$prices = array(
+    "lactasoy" => 6,
+    "mirinda" => 16,
+    "seven_up" => 18
+);
+
+$product = $_POST["product"];
+$amount = $_POST["amount"];
+$calculated_price = $amount * $prices["$product"];
+$sale_name = $_SESSION["username"];
+
+$sql = "INSERT INTO order_list VALUES (NULL, '$product', $amount, $calculated_price, '$sale_name', 'unconfirm')";
+$result = $mysqli->query($sql);
+
+if ($result) {
+    header("Location: addOrder.php?success=Add order successfully.");
+    exit();
+} else {
+    header("Location: addOrder.php?error=error #3");
+    exit();
+}
+
+$mysqli->close();
