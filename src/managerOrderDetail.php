e3cce078617d5020024d66cca737f0e40c78abef
BazkilleR <ohm.ratchaphon@gmail.com>
2023-12-02 20:15:52 +0700

    add build.css

--- src/managerOrderDetail.php
+++ src/managerOrderDetail.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../dist/output.css">
+    <link rel="stylesheet" href="../build.css" />
+    <link rel="stylesheet" href="../dist/output.css" />
     <title>product detail</title>
 </head>
 
